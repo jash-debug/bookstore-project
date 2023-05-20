cea6528036738a3097e35a0c1b72e9ac01cafbd0
jash-debug <jash-debug@github.com>
2023-05-21 00:46:32 +0300

    Add and Delete from Api

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import BookList from './BookList';
 import AddBookForm from './AddBook';
+import { getAllBooks } from '../redux/books/BooksSlice';
 
 const Books = () => {
-  const books = useSelector((state) => state.books);
+  const { books, isLoading } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const booksArray = Object.keys(books);
+  useEffect(() => {
+    dispatch(getAllBooks());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <BookList allBooks={books.books} />
+      <BookList allBooks={booksArray} />
       <AddBookForm />
     </div>
-
   );
 };
 export default Books;
